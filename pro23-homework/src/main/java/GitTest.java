4df15c7c9d7b12ac8c2732bf5cc0935b6c6a18e0
qingyu-cloud <1592547950@qq.com>
2022-10-06 15:31:18 +0800

    master push commit

--- pro23-homework/src/main/java/GitTest.java
+++ pro23-homework/src/main/java/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,Git");
         System.out.println("master,Git");
         System.out.println("hot-fix,Git");
+        System.out.println("has push");
     }
 }
